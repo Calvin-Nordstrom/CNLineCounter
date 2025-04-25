bddc4e493401e18b8d9f70e2e6bd05304329edc5
Calvin Nordstrom <calnordstrom@icloud.com>
2025-04-24 22:10:43 -0400

    refactor: created window is now centered

--- line_counter.py
+++ line_counter.py
@@ -115,5 +115,13 @@ class LineCounterApp:
 if __name__ == "__main__":
     root = tk.Tk()
     sv_ttk.set_theme(darkdetect.theme())
-    LineCounterApp(root)
+    app = LineCounterApp(root)
+
+    root.update_idletasks()
+    width = root.winfo_width()
+    height = root.winfo_height()
+    x = (root.winfo_screenwidth() // 2) - (width // 2)
+    y = (root.winfo_screenheight() // 2) - (height // 2)
+    root.geometry(f'+{x}+{y}')
+
     root.mainloop()
